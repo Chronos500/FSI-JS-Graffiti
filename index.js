751a3cfda960625db3e9ad2efc046d4b5c538057
Hansel Quiroz <championsh500@icloud.com>
2022-07-27 19:25:02 -0400

    changes

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
-// Select some elements...
+// Select some elements..
+//changed color and text placement
+let header = document.querySelector('#page-header')
+header.style.backgroundColor = "Blue"
+header.style.textAlign = "right"
+
+//dog images
+let dogImages = document.querySelectorAll('.dog-image')
+for(let i = 0; i < dogImages.length; i++){
+	dogImages[i].style.borderRadius = '50px'
+}
+
+//change border
+footer.style.borderStyle = 'solid'
